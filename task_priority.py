4678c50d40278acb0e9c92d394779d458e586bb4
hangsam nembang <h.samnembang@gmail.com>
2026-07-30 11:32:11 +1000

    added while looping for input

---
+++ task_priority.py
@@ -0,0 +1,48 @@
+# Task Tracker
+# Hangsam Nembang
+# This program gets task details from user and utilises loops
+
+#Task class
+class Task:
+    name:str = ''
+    priority:str = ''
+    time_estimate:int = 0
+    urgent:str = ''
+    status:bool = False
+    cost:float = 0.0
+        
+        
+        
+    
+#welcome message
+print(""""
+      Task Tracker
+      
+      An app to track your tasks.
+      
+      """
+)
+
+input("Press 'Enter' to continue.")
+usr_input = ''
+#get input from user about task while the user does not quit
+while True:
+    print("Enter Task Name (Enter 'quit' to exit)")
+    usr_input = input()
+    if(usr_input.lower() == 'quit'):
+        print("Session Closed. Thank you for using Task Traker.")
+        break
+    
+    new_task = Task()
+    new_task.name = usr_input
+    print("Enter Task Priority")
+    new_task.priority = input("->")
+   
+
+
+
+
+
+
+    
+    
