269c2d891071266e8ec372e9e068ae6b9bca3074
hangsam nembang <h.samnembang@gmail.com>
2026-07-31 11:55:19 +1000

    refactored task_tracker

--- task_tracker.py
+++ task_tracker.py
@@ -11,55 +11,60 @@ class Task:
     status:bool = False
     cost:float = 0.0
     
-    #print greeting for the user
-    def greet_user():
-        print(""""
-            Task Tracker
-            
-            An app to track your tasks.
-            
-            """
-        )
-        input("Press 'Enter' to continue.")
-    
-      
-    #conditional to check the priority
-    def PriorityMessage(self):
-        if(self.priority.lower() == 'high'):
-            print("This task is high priority and should be done first")
-        elif(self.priority.lower() == 'medium'):
-            print("This task should be scheduled soon")
-        elif(self.priority.lower() == 'low'):
-            print("This task should be done when time allows")
-        else:
-            print("Priority input was not recognised. Pease enter a valid priority.")
-    
-
-
-#function to get input in the form of string and return string value
-def GetInput():
-    return input("->")  
-    
-#welcome message
-
-usr_input = ''
-#get input from user about task while the user does not quit
-while True:
-    print("Enter Task Name (Enter 'quit' to exit)")
-    usr_input = GetInput()
-    if(usr_input.lower() == 'quit'):
-        print("Session Closed. Thank you for using Task Traker.")
-        break
-    #if the input is not empty, the != checks if the input doesnt equal empty string to proceeed
-    elif(usr_input != ''):
-        new_task = Task()
-        new_task.name = usr_input
+    #getter 
+    def get_task_input(self):
+        print("Enter Task Name (Enter 'quit' to exit)")
+        return input("->")
+        
+    def get_priority_input(self):
         print("Enter Task Priority")
-        new_task.priority = GetInput()
-        new_task.PriorityMessage()
-    else:
-        print("Error invalid input.")
+        return input("->")
+        
+    #conditional to check the priority collects string and returns a message based on it 
+    def PriorityMessage(self, priority:str):
+        if(priority.lower() == 'high'):
+            return("This task is high priority and should be done first")
+        elif(priority.lower() == 'medium'):
+            return("This task should be scheduled soon")
+        elif(priority.lower() == 'low'):
+            return("This task should be done when time allows")
+        else:
+            return("Priority input was not recognised. Pease enter a valid priority.")
+
+#print greeting for the user
+def greet_user():
+    print(""""
+        Task Tracker
+        
+        An app to track your tasks.
+        
+        """
+    )
+    input("Press 'Enter' to continue.")
+    
+         
+#run the program
+def run_tracker():
+    greet_user()
+
+    new_task = Task()
+
+    while True:
+        
+        new_task.name = new_task.get_task_input()
+        if(new_task.name.lower() == 'quit'):
+            print("Session Closed. Thank you for using Task Traker.")
+            break
+        #if the input is not empty, the != checks if the input doesnt equal empty string to proceeed
+        elif(new_task.name != ''):
+            new_task.priority = new_task.get_priority_input()
+            print(new_task.PriorityMessage(new_task.priority))
+        else:
+            print("Error invalid input.")
    
+
+    
+run_tracker()
 
 
 
